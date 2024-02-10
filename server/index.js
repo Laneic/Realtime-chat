9de8b0a6a0b5e9c823dea67e1f538c6f4f4615f7
Aryan Verma <119877909+Laneic@users.noreply.github.com>
2024-02-10 11:51:29 +0530

    Installing react
    Installed React

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000; // You can choose any available port
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
